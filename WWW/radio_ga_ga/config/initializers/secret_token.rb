d3284cd126d7ec18a16cafa35cd3abc7441dd197
Eugene M <jenikm@gmail.com>
2011-11-10 16:01:54 -0600

    Adding initial draft for website

---
+++ WWW/radio_ga_ga/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RadioGaGa::Application.config.secret_token = 'a06ac299cc0d680cd9e3e5dea85867234e6fad29971e741b7f58fe9550dd7d8d1274a46fe99ab0efe2ac1115fbe73c4cc8762aeb2dfc36d59e559bfe0dfda15a'
