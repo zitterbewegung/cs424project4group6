d71406c14d0659fc722e398716bb6195b40ed6fe
Joshua Herman <zitterbewegung@gmail.com>
2011-11-15 12:31:11 -0600

    SQL querys to generate entries in postgresql

---
+++ cs424-project4-lastfm/COPY1k.sql
@@ -0,0 +1,3 @@
+﻿
+create table tracks_listened (userid CHAR(11), when_listened TIMESTAMP ,artist_id CHAR(36), artist_name VARCHAR, track_id CHAR(36), track_name VARCHAR) ; 
+COPY tracks_listened from '/data/lastfm-dataset-1K/userid-timestamp-artid-artname-traid-traname.tsv'  WITH NULL AS ''; 
---
+++ cs424-project4-lastfm/cs424-360k-creation.sql
@@ -0,0 +1,6 @@
+﻿
+create table profile (user_mboxsha1 VARCHAR, gender CHAR(1),age INTEGER,country VARCHAR,signup TIMESTAMP);                                                                                       
+create table top_ten (usersha1 CHAR(40), musicbrainz_artist_id CHAR(40) ,artist VARCHAR, plays INTEGER); 
+copy top_ten from '/data/lastfm-dataset-360K/usersha1-artmbid-artname-plays.tsv' WITH NULL AS '';
+copy profile from '/data/lastfm-dataset-360K/usersha1-profile.tsv' WITH NULL AS ''; 
+create table tracks_listened (usersha1 CHAR(40), musicbrainz_artist_id CHAR(40) ,artist_name VARCHAR, musicbrainz_track_id CHAR(40), empty CHAR(1), track_name VARCHAR); 
